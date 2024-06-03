8215b1cd511a2d04666a9c6ba3c6b63e582675a1
sugishitadaichi <fusion.haa0743@outlook.jp>
2024-06-03 20:07:03 +0900

    janken_app

---
+++ README.md
@@ -0,0 +1,9 @@
+# janken_app20240603
+
+A new Flutter project.
+
+## 概要説明
+
+・使用言語：Flutter
+・このRepositoryはじゃんけんアプリ作成のために作成されました。
+・Flutter初アプリ作成です。
---
+++ android/app/src/main/kotlin/jp/haa/fusion/janken_app20240603/MainActivity.kt
@@ -0,0 +1,5 @@
+package jp.haa.fusion.janken_app20240603
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
