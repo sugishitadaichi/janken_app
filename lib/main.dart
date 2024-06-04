a9a8b5823d3706f2fb0c9e296dc66a71433bccd7
sugishitadaichi <fusion.haa0743@outlook.jp>
2024-06-04 18:43:02 +0900

    相手がランダムでじゃんけんを出す

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+//Random使用のためインポート
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -13,21 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
+
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -39,14 +27,7 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -56,32 +37,30 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+//自分のじゃんけんマスタ
+  String myJankenText = '👊';
+//相手のじゃんけんマスタ
+  String computerJankenText = '👊';
+//じゃんけんリスト（相手の）
+  List<String>jankenList = ['✌️','👊','✋'];
 
-  String jankenText = '👊';
-
-  void _chooseJankenText() {
+  void chooseComputerText() {
+    final random = Random();
+    final randomNumber = random.nextInt(3);
+    final hand = jankenList[randomNumber];
     setState(() {
-      jankenText = '✋';
-      _counter++;
+      computerJankenText = hand;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
+
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+
         title: Text(widget.title),
       ),
       body: Center(
@@ -94,7 +73,7 @@ class _MyHomePageState extends State<MyHomePage> {
               style: TextStyle(fontSize: 30),
             ),
             Text(
-              '✌️',
+              computerJankenText,
               style: TextStyle(fontSize: 100),
             ),
             SizedBox(
@@ -105,7 +84,7 @@ class _MyHomePageState extends State<MyHomePage> {
               style: TextStyle(fontSize: 30),
             ),
             Text(
-              jankenText,
+              myJankenText,
               style: TextStyle(fontSize: 200),
             )
           ],
@@ -117,8 +96,10 @@ class _MyHomePageState extends State<MyHomePage> {
           FloatingActionButton(
               onPressed:(){
                 setState(() {
-                  jankenText = '👊';
+                  myJankenText = '👊';
                 });
+                //相手のランダム選択をボタンを押すときに呼び出す
+                chooseComputerText();
               },
             child: const Text(
               '👊',
@@ -132,8 +113,10 @@ class _MyHomePageState extends State<MyHomePage> {
           FloatingActionButton(
             onPressed:(){
               setState(() {
-                jankenText = '✌️';
+                myJankenText = '✌️';
               });
+              //相手のランダム選択をボタンを押すときに呼び出す
+              chooseComputerText();
             },
             child: const Text(
               '✌️',
@@ -147,8 +130,10 @@ class _MyHomePageState extends State<MyHomePage> {
           FloatingActionButton(
             onPressed:(){
               setState(() {
-                jankenText = '✋️';
+                myJankenText = '✋️';
               });
+              //相手のランダム選択をボタンを押すときに呼び出す
+              chooseComputerText();
             },
             child: const Text(
               '✋️',
